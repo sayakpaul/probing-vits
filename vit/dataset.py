f54f9ae03a2e0f865d623a421ca86d22b09c3a05
ariG23498 <aritra.born2fly@gmail.com>
2022-03-18 10:04:09 +0530

    fix: AUTO private and module level

--- vit/dataset.py
+++ vit/dataset.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 from tensorflow import keras
 
+_AUTO = tf.data.AUTOTUNE
 
 def get_cifar_dataset(config):
     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -9,18 +10,17 @@ def get_cifar_dataset(config):
         (x_train[:40000], y_train[:40000]),
         (x_train[40000:], y_train[40000:]),
     )
-    AUTO = tf.data.AUTOTUNE
     train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     train_ds = (
         train_ds.shuffle(config.buffer_size)
         .batch(config.batch_size)
-        .prefetch(AUTO)
+        .prefetch(_AUTO)
     )
 
     val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-    val_ds = val_ds.batch(config.batch_size).prefetch(AUTO)
+    val_ds = val_ds.batch(config.batch_size).prefetch(_AUTO)
 
     test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-    test_ds = test_ds.batch(config.batch_size).prefetch(AUTO)
+    test_ds = test_ds.batch(config.batch_size).prefetch(_AUTO)
 
     return (train_ds, val_ds, test_ds)
